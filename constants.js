79e4a77b5e4634be72ea79fdba1afce060b6cff5
figsandolives <sultan.figsolives@gmail.com>
2026-02-11 15:44:50 +0300

    Delete constants.js

--- constants.js
+++
@@ -1,8 +0,0 @@
-export const BAKERY_LOCATION = {
-  name: "مخبز التين والزيتون",
-  lat: 29.3155309,
-  lng: 48.0058806
-};
-
-export const COUNTRY_CODE = "965";
-export const CONSENT_LINK_BASE = "https://figsandolives.github.io/location/consent.html?agentId=";
